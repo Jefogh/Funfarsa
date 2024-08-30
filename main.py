425274d898c349b28d251de2edd36d235c26ae8a
Jefogh <jefo221gh@gmail.com>
2024-08-30 17:15:00 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+def main(page):
+    page.title = "Flet App"
+    page.add(ft.Text("Hello, this is a Flet app running as a web app!"))
+
+if __name__ == "__main__":
+    ft.app(target=main)
